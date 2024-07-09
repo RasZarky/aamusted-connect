dc1bb5ebe412bd4c8668b1105af5d197d2158ddb
RasZarky <ubdoolrazak@gmail.com>
2024-07-09 00:56:58 +0000

    initial commit

---
+++ android/app/src/main/kotlin/com/example/aamuster_connect/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aamuster_connect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:aamuster_connect/state/appState.dart';
+import 'package:aamuster_connect/state/authState.dart';
+import 'package:aamuster_connect/state/chats/chatState.dart';
+import 'package:aamuster_connect/state/feedState.dart';
+import 'package:aamuster_connect/state/notificationState.dart';
+import 'package:aamuster_connect/state/searchState.dart';
+import 'package:aamuster_connect/state/suggestionUserState.dart';
+import 'package:aamuster_connect/ui/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'helper/routes.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // setupDependencies();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppState>(create: (_) => AppState()),
+        ChangeNotifierProvider<AuthState>(create: (_) => AuthState()),
+        ChangeNotifierProvider<FeedState>(create: (_) => FeedState()),
+        ChangeNotifierProvider<ChatState>(create: (_) => ChatState()),
+        ChangeNotifierProvider<SearchState>(create: (_) => SearchState()),
+        ChangeNotifierProvider<NotificationState>(
+            create: (_) => NotificationState()),
+        ChangeNotifierProvider<SuggestionsState>(
+            create: (_) => SuggestionsState()),
+      ],
+      child: MaterialApp(
+        title: 'Aamusted Connect',
+        theme: AppTheme.appTheme.copyWith(
+          textTheme: GoogleFonts.mulishTextTheme(
+            Theme.of(context).textTheme,
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+        routes: Routes.route(),
+        onGenerateRoute: (settings) => Routes.onGenerateRoute(settings),
+        onUnknownRoute: (settings) => Routes.onUnknownRoute(settings),
+        initialRoute: "SplashPage",
+      ),
+    );
+  }
+}
